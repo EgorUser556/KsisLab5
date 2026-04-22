ef5d12552a76620e298e4cade68bc9b71a38c3f3
EgorUser556 <loadend33@gmail.com>
2026-04-22 20:27:07 +0300

    fix: Store class changes

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StorageApplication {
+public class Store {
     public static void main(String[] args) {
-        SpringApplication.run(StorageApplication.class, args);
+        SpringApplication.run(Store.class, args);
     }
 }
